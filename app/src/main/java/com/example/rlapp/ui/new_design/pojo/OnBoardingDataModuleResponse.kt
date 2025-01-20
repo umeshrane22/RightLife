d686af349973f7f7edbb88de1f9cf1062ce5f551
sudhir14git <sudhier.joshi@gmail.com>
2025-01-20 22:54:17 +0530

    onboarding

--- app/src/main/java/com/example/rlapp/ui/new_design/pojo/OnBoardingDataModuleResponse.kt
+++ app/src/main/java/com/example/rlapp/ui/new_design/pojo/OnBoardingDataModuleResponse.kt
@@ -3,7 +3,6 @@ package com.example.rlapp.ui.new_design.pojo
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-/*
 class OnBoardingDataModuleResponse {
     @SerializedName("success")
     @Expose
@@ -16,18 +15,4 @@ class OnBoardingDataModuleResponse {
     @SerializedName("data")
     @Expose
     var data: OnboardingModuleData? = null
-}*/
-
-class OnBoardingDataModuleResponse {
-    @SerializedName("success")
-    @Expose
-    var success: Boolean? = null
-
-    @SerializedName("statusCode")
-    @Expose
-    var statusCode: Int? = null
-
-    @SerializedName("data")
-    @Expose
-    var data: List<OnboardingModuleData>? = null
 }
