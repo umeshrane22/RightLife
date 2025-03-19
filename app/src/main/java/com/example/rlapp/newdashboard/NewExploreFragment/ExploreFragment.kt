c779d5334745e76594c7ac7ef6effde560a5c87f
sudhir14git <sudhier.joshi@gmail.com>
2025-03-19 14:12:27 +0530

    breathing started new quick links menu

---
+++ app/src/main/java/com/example/rlapp/newdashboard/NewExploreFragment/ExploreFragment.kt
@@ -0,0 +1,17 @@
+package com.example.rlapp.newdashboard.NewExploreFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.rlapp.R
+
+class ExploreFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_explore, container, false)
+    }
+}
