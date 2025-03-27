83e3a17359072ffcd50054db1e0de435737bfd7a
sudhir14git <sudhier.joshi@gmail.com>
2025-03-27 19:50:26 +0530

    Custom Ui for Questionaire

--- app/src/main/java/com/example/rlapp/ui/questionnaire/fragment/EmotionsPastWeekFragment.kt
+++ app/src/main/java/com/example/rlapp/ui/questionnaire/fragment/EmotionsPastWeekFragment.kt
@@ -4,12 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
+import com.example.rlapp.R
 import com.example.rlapp.databinding.FragmentEmotionsPastWeekBinding
 import com.example.rlapp.ui.questionnaire.QuestionnaireEatRightActivity
 import com.example.rlapp.ui.questionnaire.pojo.Question
 
 class EmotionsPastWeekFragment : Fragment() {
+
+
+    private lateinit var emotionSelected: String
+    private val EmotionsList = arrayOf(
+        "Chaotic",
+        "UnStable",
+        "Uneven",
+        "Steady",
+        "Calm"
+
+    )
+    private val EmotionsBgCards = arrayOf(
+        R.drawable.chaotic_bg_card,
+        R.drawable.unstable_bg_card,
+        R.drawable.uneven_bg_card,
+        R.drawable.steady_bg_card,
+        R.drawable.calm_bg_card
+
+    )
 
     private var _binding: FragmentEmotionsPastWeekBinding? = null
     private val binding get() = _binding!!
@@ -46,6 +67,22 @@ class EmotionsPastWeekFragment : Fragment() {
         binding.btnContinue.setOnClickListener {
             QuestionnaireEatRightActivity.navigateToNextPage()
         }
+
+        //default color
+        binding.tvTitle.text = EmotionsList[0]
+        emotionSelected =  EmotionsList[0]
+        binding.cardView.setBackgroundResource(EmotionsBgCards[0])
+        // Set SeekBar listener to change background color dynamically
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.tvTitle.text = EmotionsList[progress]
+                emotionSelected = EmotionsList[progress]
+                binding.cardView.setBackgroundResource(EmotionsBgCards[progress])
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/rlapp/ui/questionnaire/fragment/WaterCaffeineIntakeFragment.kt
+++ app/src/main/java/com/example/rlapp/ui/questionnaire/fragment/WaterCaffeineIntakeFragment.kt
@@ -1,16 +1,33 @@
 package com.example.rlapp.ui.questionnaire.fragment
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.rlapp.R
 import com.example.rlapp.databinding.FragmentWaterCaffeineIntakeBinding
 import com.example.rlapp.ui.questionnaire.QuestionnaireEatRightActivity
 import com.example.rlapp.ui.questionnaire.pojo.Question
 
 class WaterCaffeineIntakeFragment : Fragment() {
 
+    var intervalColors: IntArray = intArrayOf(
+       R.color.red,  // 0-2k
+        Color.parseColor("#F8F836"),  // 2-4k
+        Color.parseColor("#84D348"),  // 4-6k
+        Color.parseColor("#49A2DB"),  // 6-8k
+        Color.parseColor("#49A2DB"),  // 8-10k
+        Color.parseColor("#49A2DB") // 10-12k
+    )
+
+
+    private lateinit var waterCountTexts: Array<TextView>
+    private lateinit var coffeeCountTexts: Array<TextView>
     private var _binding: FragmentWaterCaffeineIntakeBinding? = null
     private val binding get() = _binding!!
 
@@ -44,9 +61,68 @@ class WaterCaffeineIntakeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        waterCountTexts = arrayOf<TextView>(
+            binding.waterCountText1,
+            binding.waterCountText2,
+            binding.waterCountText3,
+            binding.waterCountText4,
+            binding.waterCountText5,
+            binding.waterCountText6
+        )
+
+        coffeeCountTexts = arrayOf<TextView>(
+            binding.coffeeCountText1,
+            binding.coffeeCountText2,
+            binding.coffeeCountText3,
+            binding.coffeeCountText4
+
+        )
 
         binding.btnContinue.setOnClickListener {
             QuestionnaireEatRightActivity.navigateToNextPage()
+        }
+        binding.waterView.setMinSteps(0)
+        binding.waterView.setMaxSteps(12000)
+        binding.waterView.setIntervalColors(ContextCompat.getColor(requireContext(), R.color.water_dark_color))
+        binding.waterView.setOnStepCountChangeListener { stepCount ->
+
+            // Reset all TextViews to normal
+            for (textView in waterCountTexts) {
+                textView.setTypeface(null, Typeface.NORMAL)
+            }
+            // Determine which TextView to bold based on stepCount
+            val index = stepCount / 2000 - 1
+            if (index >= 0 && index < waterCountTexts.size) {
+                waterCountTexts.get(index).setTypeface(null, Typeface.BOLD)
+                binding.tvWaterQuantity.text = (500*(index+1)).toString() + " ml"
+            }
+        }
+
+        binding.coffeeView.setMinSteps(0)
+        binding.coffeeView.setMaxSteps(8000)
+        binding.coffeeView.setIntervalColors(ContextCompat.getColor(requireContext(), R.color.coffee_dark_color))
+        //binding.coffeeView.setStepIntervalCount(1000)
+        binding.coffeeView.setOnStepCountChangeListener { stepCount ->
+            //binding.coffeeView.setStepIntervalCount(2000)
+
+            // Reset all TextViews to normal
+            for (textView in coffeeCountTexts) {
+                textView.setTypeface(null, Typeface.NORMAL)
+            }
+            // Determine which TextView to bold based on stepCount
+            val index = stepCount / 2000 - 1
+            if (index >= 0 && index < coffeeCountTexts.size) {
+                coffeeCountTexts.get(index).setTypeface(null, Typeface.BOLD)
+                if (index==0) {
+                    binding.tvCoffeeQuantity.text = (125 *1).toString() + " ml"
+                }else if (index==1){
+                    binding.tvCoffeeQuantity.text =  (125 *3).toString() + " ml"
+                }else if (index==2){
+                    binding.tvCoffeeQuantity.text =  (125 *5).toString() + " ml"
+                }else if (index==3){
+                    binding.tvCoffeeQuantity.text =  (125 *7).toString() + " ml"
+                }
+            }
         }
     }
 
