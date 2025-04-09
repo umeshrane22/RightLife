2d84f813550d7e0cb55da32db807d2188e137725
sudhir14git <sudhier.joshi@gmail.com>
2025-04-09 10:35:09 +0530

    big fixes 7 april

--- app/src/main/java/com/jetsynthesys/rightlife/ui/mindaudit/MindAuditResultActivity.kt
+++ app/src/main/java/com/jetsynthesys/rightlife/ui/mindaudit/MindAuditResultActivity.kt
@@ -62,14 +62,38 @@ class MindAuditResultActivity : AppCompatActivity() {
                 response: Response<MindAuditResultResponse?>
             ) {
                 if (response.isSuccessful && response.body() != null) {
+              /*      try {
+                        if (response.body()!!.result.isNullOrEmpty()){
+
+                            binding.rlAssessmentNotTaken.visibility = View.GONE
+                            binding.scrollviewResult.visibility = View.VISIBLE
+                        // binding.tvMainScore.text = response.body()!!.result[0].assessmentsTaken[0].interpretations.anger.level.toString() + " " + response.body()!!.result[0].assessmentsTaken[0].interpretations.anger.score.toString()
+                            handleAssessmentScore(response)
+                        }else{
+                            binding.rlAssessmentNotTaken.visibility = View.VISIBLE
+                            binding.scrollviewResult.visibility = View.GONE
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }*/
+
                     try {
-                        binding.tvScore.text =
-                            response.body()!!.result[0].assessmentsTaken[0].interpretations.anger.level.toString() + " " + response.body()!!.result[0].assessmentsTaken[0].interpretations.anger.score.toString()
+                        val resultList = response.body()?.result
+
+                        if (!resultList.isNullOrEmpty()) {
+                            // Result exists, show result layout
+                            binding.rlAssessmentNotTaken.visibility = View.GONE
+                            binding.scrollviewResult.visibility = View.VISIBLE
+
+                            handleAssessmentScore(response)
+                        } else {
+                            // No result, show "not taken" layout
+                            binding.rlAssessmentNotTaken.visibility = View.VISIBLE
+                            binding.scrollviewResult.visibility = View.GONE
+                        }
                     } catch (e: Exception) {
                         e.printStackTrace()
                     }
-
-
                 } else {
                     Toast.makeText(
                         this@MindAuditResultActivity,
@@ -87,6 +111,169 @@ class MindAuditResultActivity : AppCompatActivity() {
                 ).show()
             }
         })
+    }
+
+
+    private fun handleAssessmentScore(response: Response<MindAuditResultResponse?>) {
+        var assessmentTaken = response.body()!!.result[0].assessmentsTaken[0]
+        with(binding) {
+            when (assessmentTaken.assessment) {
+                "DASS-21" -> {
+                    assessmentTaken.interpretations.anxiety?.let {
+                        cardviewMainscore.visibility = View.VISIBLE
+                        binding.mainScoreTitle.text = "Anxiety"
+                        binding.mainScoreTitle.visibility = View.VISIBLE
+                        binding.mainScoreLevel.text = assessmentTaken.interpretations.anxiety.level
+                        binding.mainScoreLevel.visibility = View.VISIBLE
+                        binding.tvMainScore.text = assessmentTaken.interpretations.anxiety.score.toString()
+                        binding.cardviewMainscore.setCardBackgroundColor(getResources().getColor(getColorResForScore(assessmentTaken.interpretations.anxiety.score)))
+                        setExplanationTitle(assessmentTaken.interpretations.anxiety.level,"Anxiety")
+                        setRainbowView(assessmentTaken.interpretations.anxiety.score.toInt())
+                        //setLeftRainbowView(assessmentTaken.interpretations.anxiety.score.toInt())
+                    }
+                    assessmentTaken.interpretations.depression?.let {
+                        cardviewMainscore2.visibility = View.VISIBLE
+                        binding.mainScoreTitle2.text = "Depression"
+                        binding.mainScoreTitle2.visibility = View.VISIBLE
+                        binding.mainScoreLevel2.text = assessmentTaken.interpretations.depression.level
+                        binding.mainScoreLevel2.visibility = View.VISIBLE
+                        binding.tvMainScore2.text = assessmentTaken.interpretations.depression.score.toString()
+                        setExplanationTitle(assessmentTaken.interpretations.depression.level,"Depression")
+                        binding.cardviewMainscore2.setCardBackgroundColor(getResources().getColor(getColorResForScore(assessmentTaken.interpretations.depression.score)))
+                        //setCenterRainbowView(assessmentTaken.interpretations.depression.score.toInt())
+                    }
+                    assessmentTaken.interpretations.stress?.let {
+                        cardviewMainscore3.visibility = View.VISIBLE
+                        binding.mainScoreTitle3.text = "Stress"
+                        binding.mainScoreTitle3.visibility = View.VISIBLE
+                        binding.mainScoreLevel3.text = assessmentTaken.interpretations.stress.level
+                        binding.mainScoreLevel3.visibility = View.VISIBLE
+                        binding.tvMainScore3.text = assessmentTaken.interpretations.stress.score.toString()
+                        binding.cardviewMainscore3.setCardBackgroundColor(getResources().getColor(getColorResForScore(assessmentTaken.interpretations.stress.score)))
+                        setExplanationTitle(assessmentTaken.interpretations.stress.level,"Stress")
+                        //setRightRainbowView(assessmentTaken.interpretations.stress.score.toInt())
+                    }
+                    scoreBarContainer.visibility = View.VISIBLE
+                    scoreBarContainerhappiness.visibility = View.GONE
+                }
+
+                "PHQ-9" -> {
+                    assessmentTaken.interpretations.depression?.let {
+                        binding.mainScoreTitle.text = "Depression"
+                        binding.mainScoreTitle.visibility = View.VISIBLE
+                        binding.mainScoreLevel.text = assessmentTaken.interpretations.depression.level
+                        binding.mainScoreLevel.visibility = View.VISIBLE
+                        binding.tvMainScore.text = assessmentTaken.interpretations.depression.score.toString()
+                        setRainbowView(assessmentTaken.interpretations.depression.score.toInt())
+                        binding.cardviewMainscore.setCardBackgroundColor(getResources().getColor(getColorResForScore(assessmentTaken.interpretations.depression.score)))
+                        setExplanationTitle(assessmentTaken.interpretations.depression.level,"Depression")
+                    }
+                    cardviewMainscore3.visibility = View.GONE
+                    cardviewMainscore2.visibility = View.GONE
+                    scoreBarContainer.visibility = View.VISIBLE
+                    scoreBarContainerhappiness.visibility = View.GONE
+                }
+
+                "OHQ" -> {
+                    assessmentTaken.interpretations.happiness?.let {
+                        binding.mainScoreTitle.text = "Happiness"
+                        binding.mainScoreTitle.visibility = View.VISIBLE
+                        binding.mainScoreLevel.text = assessmentTaken.interpretations.happiness.level
+                        binding.mainScoreLevel.visibility = View.VISIBLE
+                        binding.tvMainScore.text = assessmentTaken.interpretations.happiness.score.toString()
+                        setHappinessRainbowView(assessmentTaken.interpretations.happiness.score.toInt())
+                        binding.cardviewMainscore.setCardBackgroundColor(getResources().getColor(getColorResForScore(assessmentTaken.interpretations.happiness.score.toInt())))
+                        setExplanationTitle(assessmentTaken.interpretations.happiness.level,"Happiness")
+                    }
+                    cardviewMainscore3.visibility = View.GONE
+                    cardviewMainscore2.visibility = View.GONE
+                    scoreBarContainer.visibility = View.GONE
+                    scoreBarContainerhappiness.visibility = View.VISIBLE
+                }
+
+                "GAD-7" -> {
+                    assessmentTaken.interpretations.anxiety?.let {
+                        binding.mainScoreTitle.text = "Anxiety"
+                        binding.mainScoreTitle.visibility = View.VISIBLE
+                        binding.mainScoreLevel.text = assessmentTaken.interpretations.anxiety.level
+                        binding.mainScoreLevel.visibility = View.VISIBLE
+                        binding.tvMainScore.text =
+                            assessmentTaken.interpretations.anxiety.score.toString()
+                        setRainbowView(assessmentTaken.interpretations.anxiety.score)
+                        binding.cardviewMainscore.setCardBackgroundColor(getResources().getColor(getColorResForScore(assessmentTaken.interpretations.anxiety.score)))
+                        setExplanationTitle(assessmentTaken.interpretations.anxiety.level,"Anxiety")
+                    }
+                    cardviewMainscore3.visibility = View.GONE
+                    cardviewMainscore2.visibility = View.GONE
+                    scoreBarContainer.visibility = View.VISIBLE
+                    scoreBarContainerhappiness.visibility = View.GONE
+                }
+
+                "CAS" -> {
+                    assessmentTaken.interpretations.anger?.let {
+                        binding.mainScoreTitle.text = "Anger"
+                        binding.mainScoreTitle.visibility = View.VISIBLE
+                        binding.mainScoreLevel.text = assessmentTaken.interpretations.anger.level
+                        binding.mainScoreLevel.visibility = View.VISIBLE
+                        binding.tvMainScore.text = assessmentTaken.interpretations.anger.score.toString()
+                        setRainbowView(assessmentTaken.interpretations.anger.score)
+                        binding.cardviewMainscore.setCardBackgroundColor(getResources().getColor(getColorResForScore(assessmentTaken.interpretations.anger.score)))
+                        setExplanationTitle(assessmentTaken.interpretations.anger.level,"Anger")
+                    }
+                    cardviewMainscore3.visibility = View.GONE
+                    cardviewMainscore2.visibility = View.GONE
+                    scoreBarContainer.visibility = View.VISIBLE
+                    scoreBarContainerhappiness.visibility = View.GONE
+                }
+
+                else -> {}
+            }
+        }
+
+
+    }
+
+    private fun setLeftRainbowView(score: Int) {
+        binding.leftArc.visibility = View.VISIBLE
+        binding.leftArc.setRainbowColors(getColorArrayForScore(score))
+        binding.leftArc.setStrokeWidth(30f)
+        binding.leftArc.setArcSpacing(8f)
+        binding.leftArc.startAngle = 180f
+        binding.leftArc.sweepAngle = 90f
+        binding.leftArc.invalidate()
+    }
+    private fun setRightRainbowView(score: Int) {
+        binding.rightArc.visibility = View.VISIBLE
+        binding.rightArc.setRainbowColors(getColorArrayForScore(score))
+        binding.rightArc.setStrokeWidth(30f)
+        binding.rightArc.setArcSpacing(8f)
+        binding.rightArc.startAngle = 270f
+        binding.rightArc.sweepAngle = 90f
+    }
+    private fun setCenterRainbowView(score: Int) {
+        binding.centerArc.visibility = View.VISIBLE
+        binding.centerArc.setRainbowColors(getColorArrayForScore(score))
+        binding.centerArc.setStrokeWidth(30f)
+        binding.centerArc.setArcSpacing(8f)
+        binding.centerArc.startAngle = 180f
+        binding.centerArc.sweepAngle = 180f
+    }
+
+    private fun setRainbowView(score: Int) {
+        binding.rainbowView.visibility = View.VISIBLE
+        binding.rainbowView.setRainbowColors(getColorArrayForScore(score))
+        binding.rainbowView.setStrokeWidth(60f)
+        binding.rainbowView.setArcSpacing(8f)
+    }
+    private fun setHappinessRainbowView(score: Int) {
+        binding.rainbowView.visibility = View.VISIBLE
+        binding.rainbowView.setRainbowColors(getColorArrayForHappinessScore(7))
+        binding.rainbowView.setStrokeWidth(40f)
+        binding.rainbowView.setArcSpacing(8f)
+    }
+
+    private fun setExplanationTitle(level: String?, title: String) {
+        binding.tvResultExplanationTitle.text = level+" "+title
     }
 
     //get all assesments
@@ -248,4 +435,77 @@ class MindAuditResultActivity : AppCompatActivity() {
         chip.setTextColor(textColorStateList)
         binding.chipGroup1.addView(chip)
     }
+
+    fun getColorResForScore(score: Int): Int {
+        return when (score) {
+            in 0..3 -> R.color.green_minimal
+            in 4..8 -> R.color.cyan_mild
+            in 9..13 -> R.color.blue_moderate
+            in 14..18 -> R.color.orange_severe
+            else -> R.color.red_ext_severe
+        }
+    }
+
+
+    fun getColorArrayForScore(score: Int): IntArray {
+        val colorLevels = listOf(
+            0xFF06B27B.toInt(), // green_minimal
+            0xFF54C8DB.toInt(), // cyan_mild
+            0xFF57A3FC.toInt(), // blue_moderate
+            0xFFFFBD44.toInt(), // orange_severe
+            0xFFFC6656.toInt()  // red_ext_severe
+        )
+
+        val fallbackColor = 0xFFEFF0F6.toInt() // light gray for unfilled slots
+
+        val activeColorCount = when (score) {
+            in 0..3 -> 1
+            in 4..8 -> 2
+            in 9..13 -> 3
+            in 14..18 -> 4
+            else -> 5
+        }
+
+        return IntArray(5) { index ->
+            if (index < activeColorCount) colorLevels[index] else fallbackColor
+        }
+    }
+
+    fun getColorArrayForHappinessScore(score: Int): IntArray {
+        /*val colorLevels = listOf(
+            0xFF06B27B.toInt(), // green_minimal
+            0xFF54C8DB.toInt(), // cyan_mild
+            0xFF57A3FC.toInt(), // blue_moderate
+            0xFFFFBD44.toInt(), // orange_severe
+            0xFFFC6656.toInt()  // red_ext_severe
+        )*/
+        //#F7E6B7,#FDE6FF,#FDD3D2,#A6E0CE,#C5DEF9,#D5C6ED,#D9DBE9
+        val colorLevels = listOf(
+            0xFFD9DBE9.toInt(), // pastel lavender
+            0xFFD5C6ED.toInt(), // soft lilac
+            0xFFC5DEF9.toInt(), // light sky blue
+            0xFFA6E0CE.toInt(), // mint green
+            0xFFFDD3D2.toInt(), // soft peach
+            0xFFFDE6FF.toInt(), // light pink-lavender
+            0xFFF7E6B7.toInt()  // soft beige
+        )
+
+
+        val fallbackColor = 0xFFEFF0F6.toInt() // light gray for unfilled slots
+
+        val activeColorCount = when (score) {
+            in 0..2 -> 1
+            in 2..3 -> 2
+            in 3..4 -> 3
+            in 4..5 -> 4
+            in 5..6 -> 5
+            in 6..7 -> 6
+            else -> 7
+        }
+
+        return IntArray(7) { index ->
+            if (index < activeColorCount) colorLevels[index] else fallbackColor
+        }
+    }
+
 }
