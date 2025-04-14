2b1f2e2eaae14986b635009f21e0fc62fe2d8349
sudhir14git <sudhier.joshi@gmail.com>
2025-04-14 19:27:39 +0530

    Onboarding Chnages  - Show only first name on screen.  - Happy to have you screen added  - Sync now screen moved after Questions  -

--- app/src/main/java/com/jetsynthesys/rightlife/ui/new_design/EnableNotificationActivity.kt
+++ app/src/main/java/com/jetsynthesys/rightlife/ui/new_design/EnableNotificationActivity.kt
@@ -40,7 +40,7 @@ class EnableNotificationActivity : AppCompatActivity() {
             }
             sharedPreferenceManager.loggedUserList
             sharedPreferenceManager.clearOnboardingData()
-            startActivity(Intent(this, FreeTrialServiceActivity::class.java))
+            startActivity(Intent(this, SyncNowActivity::class.java))
         }
     }
 }
