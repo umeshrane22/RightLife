3fb0ca14c7b56dfe3c66091190e4c3ea883b0c6d
sudhir14git <sudhier.joshi@gmail.com>
2025-04-19 15:19:41 +0530

    added used count in facescan api excercisefragment issue decimal point handling on report list

--- app/src/main/java/com/jetsynthesys/rightlife/ui/healthcam/HealthCamVitalsAdapter.java
+++ app/src/main/java/com/jetsynthesys/rightlife/ui/healthcam/HealthCamVitalsAdapter.java
@@ -1,8 +1,8 @@
 package com.jetsynthesys.rightlife.ui.healthcam;
 
-import android.content.res.ColorStateList;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -12,8 +12,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.jetsynthesys.rightlife.R;
 import com.jetsynthesys.rightlife.apimodel.newreportfacescan.HealthCamItem;
 import com.jetsynthesys.rightlife.databinding.HealthCamVitalsItemBinding;
-import com.jetsynthesys.rightlife.ui.utility.Utils;
 import com.jetsynthesys.rightlife.newdashboard.FacialScanReportDetailsActivity;
+import com.jetsynthesys.rightlife.ui.utility.ConversionUtils;
+import com.jetsynthesys.rightlife.ui.utility.Utils;
 
 import java.util.List;
 
@@ -38,7 +39,7 @@ public class HealthCamVitalsAdapter extends RecyclerView.Adapter<HealthCamVitals
     @Override
     public void onBindViewHolder(@NonNull HealthCamVitalsViewHolder holder, int position) {
         HealthCamItem item = healthCamItems.get(position);
-
+        String formatedValue = getFormatedValue(item.fieldName,String.valueOf(item.value));
         holder.binding.valueTextView.setText(String.valueOf(item.value));
         holder.binding.unitTextView.setText(item.unit);
         holder.binding.indicatorTextView.setText(item.indicator);
@@ -66,6 +67,7 @@ public class HealthCamVitalsAdapter extends RecyclerView.Adapter<HealthCamVitals
         // ... set other indicator conditions and colors as needed
 
     }
+
     private int getReportIconByType(String type) {
         switch (type) {
             case "BMI_CALC":
@@ -100,79 +102,39 @@ public class HealthCamVitalsAdapter extends RecyclerView.Adapter<HealthCamVitals
             this.binding = binding;
         }
     }
-}
 
 
+    private String getFormatedValue(String key, String value) {
+        try {
+            double val = Double.parseDouble(value);
 
-/*
-package com.example.rlapp.ui.healthcam;
+            switch (key) {
+                case "HRV_SDNN":
+                case "PHYSIO_SCORE":
+                case "BMI_CALC":
+                case "BP_CVD":
+                    return ConversionUtils.decimalFormat1Decimal.format(val);
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+                case "heartRateVariability":
+                case "waistToHeight":
 
-import com.example.rlapp.R;
-import com.example.rlapp.apimodel.newreportfacescan.HealthCamItem;
+                case "systolic":
+                case "BP_RPP":
+                case "BP_SYSTOLIC":
+                    return ConversionUtils.decimalFormat2Decimal.format(val);
 
-import java.util.List;
+                case "IHB_COUNT":
+                case "MENTAL_SCORE":
+                case "BP_DIASTOLIC":
+                    return ConversionUtils.decimalFormat0Decimal.format(val);
 
-public class HealthCamVitalsAdapter extends RecyclerView.Adapter<HealthCamVitalsAdapter.HealthCamVitalsViewHolder> {
-
-    private List<HealthCamItem> healthCamItems;
-
-    public HealthCamVitalsAdapter(List<HealthCamItem> healthCamItems) {
-        this.healthCamItems = healthCamItems;
-    }
-
-    @NonNull
-    @Override
-    public HealthCamVitalsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.health_cam_vitals_item, parent, false);
-        return new HealthCamVitalsViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull HealthCamVitalsViewHolder holder, int position) {
-        HealthCamItem item = healthCamItems.get(position);
-
-        holder.fieldName.setText(item.fieldName);
-        holder.parameter.setText(item.parameter);
-        holder.value.setText(String.valueOf(item.value));
-        holder.unit.setText(item.unit);
-        holder.indicator.setText(item.indicator);
-        holder.definition.setText(item.deffination); // Definition
-        holder.implication.setText(item.implication); // Implication
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return healthCamItems.size();
-    }
-
-    public static class HealthCamVitalsViewHolder extends RecyclerView.ViewHolder {
-        TextView fieldName;
-        TextView parameter;
-        TextView value;
-        TextView unit;
-        TextView indicator;
-        TextView definition; // Definition
-        TextView implication; // Implication
-
-
-        public HealthCamVitalsViewHolder(@NonNull View itemView) {
-            super(itemView);
-            fieldName = itemView.findViewById(R.id.fieldName);
-            parameter = itemView.findViewById(R.id.parameter);
-            value = itemView.findViewById(R.id.value);
-            unit = itemView.findViewById(R.id.unit);
-            indicator = itemView.findViewById(R.id.indicator);
-            definition = itemView.findViewById(R.id.definition); // Initialize
-            implication = itemView.findViewById(R.id.implication); // Initialize
-
+                default:
+                    return ConversionUtils.decimalFormat1Decimal.format(val);
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return value;
         }
     }
-}*/
+
+}
