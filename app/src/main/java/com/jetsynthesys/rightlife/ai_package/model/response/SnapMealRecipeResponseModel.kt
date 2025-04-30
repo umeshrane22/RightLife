9b38e94dc4b15906c6526adeae954b960da27847
“Pushpendra <pushpendra.rajput86@gmail.com>
2025-04-30 22:50:48 +0530

    complete eatright landing page, log list ui

--- app/src/main/java/com/jetsynthesys/rightlife/ai_package/model/response/SnapMealRecipeResponseModel.kt
+++ app/src/main/java/com/jetsynthesys/rightlife/ai_package/model/response/SnapMealRecipeResponseModel.kt
@@ -2,12 +2,19 @@ package com.jetsynthesys.rightlife.ai_package.model.response
 
 data class SnapMealRecipeResponseModel(
     val success: Boolean,
-    val statusCode: Int,
+    val message: String,
+    val status_code: Int,
     val data: List<SnapRecipeList>
 )
 
 data class SnapRecipeList(
     val id: String,
     val name: String,
-    val photo_url: String
+    val photo_url: String,
+    val servings: Int,
+    val cooking_time_in_seconds: Double,
+    val calories: Double,
+    val meal_type: String,
+    val food_type: String,
+    val cuisine: String
 )
