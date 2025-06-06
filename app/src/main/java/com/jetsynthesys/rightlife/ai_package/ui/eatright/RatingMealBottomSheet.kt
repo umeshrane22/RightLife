c587c41f472ea246b7f24658566fdb25747a8d52
“Pushpendra <pushpendra.rajput86@gmail.com>
2025-06-06 15:52:05 +0530

    snap meal check

--- app/src/main/java/com/jetsynthesys/rightlife/ai_package/ui/eatright/RatingMealBottomSheet.kt
+++ app/src/main/java/com/jetsynthesys/rightlife/ai_package/ui/eatright/RatingMealBottomSheet.kt
@@ -1,6 +1,7 @@
 package com.jetsynthesys.rightlife.ai_package.ui.eatright
 
 import android.R.color.transparent
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -15,15 +16,29 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.jetsynthesys.rightlife.R
-import com.jetsynthesys.rightlife.ai_package.ui.eatright.fragment.DeleteSnapMealBottomSheet
 
 class RatingMealBottomSheet: BottomSheetDialogFragment() {
+
+    private var listener: RatingSnapMealListener? = null
     private var isRating:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
+
+    interface RatingSnapMealListener {
+        fun onSnapMealRating(rating: Double)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = when {
+            context is RatingSnapMealListener -> context
+            parentFragment is RatingSnapMealListener -> parentFragment as RatingSnapMealListener
+            else -> throw ClassCastException("$context must implement RatingSnapMealListener")
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,11 +54,11 @@ class RatingMealBottomSheet: BottomSheetDialogFragment() {
         dialog.setContentView(R.layout.fragment_meal_scan_results)
         dialog.window?.setBackgroundDrawableResource(transparent)
         val closeIcon = view.findViewById<ImageView>(R.id.closeIc)
-        val layoutCancel = view.findViewById<LinearLayoutCompat>(R.id.layoutCancel)
+        val layoutSubmit = view.findViewById<LinearLayoutCompat>(R.id.layoutSubmit)
         val ratingLayout = view.findViewById<ConstraintLayout>(R.id.rating_snap_meal_layout)
         val successLayout = view.findViewById<ConstraintLayout>(R.id.rate_successfull_layout)
         val disabledLayoutCancel = view.findViewById<LinearLayoutCompat>(R.id.disabled_layoutCancel)
-        val layoutDelete = view.findViewById<LinearLayoutCompat>(R.id.layoutDelete)
+        val layoutNotNow = view.findViewById<LinearLayoutCompat>(R.id.layoutNotNow)
         val ratingBar = view.findViewById<RatingBar>(R.id.ratingBar)
       /*  if (isRating){
             layoutCancel.visibility = View.VISIBLE
@@ -56,13 +71,13 @@ class RatingMealBottomSheet: BottomSheetDialogFragment() {
             layoutCancel.isEnabled = false
             //disabledLayoutCancel.visibility =View.VISIBLE
         }*/
-        layoutCancel.setBackgroundResource(R.drawable.add_card_background_disabled)
-        layoutCancel.isEnabled = false
+        layoutSubmit.setBackgroundResource(R.drawable.add_card_background_disabled)
+        layoutSubmit.isEnabled = false
 
         ratingBar.setOnRatingBarChangeListener { _, rating, _ ->
             isRating = true
-            layoutCancel.setBackgroundResource(R.drawable.add_cart_button_background)
-            layoutCancel.isEnabled = true
+            layoutSubmit.setBackgroundResource(R.drawable.add_cart_button_background)
+            layoutSubmit.isEnabled = true
             Toast.makeText(context, "You rated $rating stars", Toast.LENGTH_SHORT).show()
         }
 
@@ -70,23 +85,28 @@ class RatingMealBottomSheet: BottomSheetDialogFragment() {
             dismiss()
         }
 
-        layoutCancel.setOnClickListener {
-            ratingLayout.visibility =View.GONE
+        layoutSubmit.setOnClickListener {
+            ratingLayout.visibility = View.GONE
             successLayout.visibility= View.VISIBLE
-
             //dismiss()
+            listener?.onSnapMealRating(1.0)
         }
 
-        layoutDelete.setOnClickListener {
+        layoutNotNow.setOnClickListener {
             dismiss()
-            Toast.makeText(view.context, "Dish Removed", Toast.LENGTH_SHORT).show()
+         //   Toast.makeText(view.context, "Dish Removed", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
     }
 
     companion object {
         const val TAG = "LoggedBottomSheet"
         @JvmStatic
-        fun newInstance() = DeleteSnapMealBottomSheet().apply {
+        fun newInstance() = RatingMealBottomSheet().apply {
             arguments = Bundle().apply {
                 // Add any required arguments here
             }
--- app/src/main/java/com/jetsynthesys/rightlife/ai_package/ui/eatright/fragment/MealScanResultFragment.kt
+++ app/src/main/java/com/jetsynthesys/rightlife/ai_package/ui/eatright/fragment/MealScanResultFragment.kt
@@ -49,6 +49,7 @@ import com.jetsynthesys.rightlife.ai_package.model.response.Macros
 import com.jetsynthesys.rightlife.ai_package.model.response.MealUpdateResponse
 import com.jetsynthesys.rightlife.ai_package.model.response.Micros
 import com.jetsynthesys.rightlife.ai_package.model.response.Nutrients
+import com.jetsynthesys.rightlife.ai_package.model.response.RegularRecipeEntry
 import com.jetsynthesys.rightlife.ai_package.model.response.SearchResultItem
 import com.jetsynthesys.rightlife.ai_package.model.response.SnapMealLogResponse
 import com.jetsynthesys.rightlife.ai_package.model.response.SnapRecipeData
@@ -79,7 +80,7 @@ import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
-class MealScanResultFragment: BaseFragment<FragmentMealScanResultsBinding>() {
+class MealScanResultFragment: BaseFragment<FragmentMealScanResultsBinding>(), RatingMealBottomSheet.RatingSnapMealListener {
 
     private lateinit var macroItemRecyclerView : RecyclerView
     private lateinit var microItemRecyclerView : RecyclerView
@@ -367,14 +368,8 @@ class MealScanResultFragment: BaseFragment<FragmentMealScanResultsBinding>() {
           //  Toast.makeText(context, "Added To Log", Toast.LENGTH_SHORT).show()
             val moduleName = arguments?.getString("ModuleName").toString()
             if (moduleName.contentEquals("EatRight")){
-                val ratingMealBottomSheet = RatingMealBottomSheet()
-                ratingMealBottomSheet.isCancelable = true
-                val bundle = Bundle()
-                bundle.putBoolean("test",false)
-                ratingMealBottomSheet.arguments = bundle
-                activity?.supportFragmentManager?.let { ratingMealBottomSheet.show(it, "RatingMealBottomSheet") }
-                sharedPreferenceManager.setFirstTimeUserForSnapMealRating(true)
-
+               // ratingMealLogDialog()
+               // sharedPreferenceManager.setFirstTimeUserForSnapMealRating(true)
                 if (mealId != "null" && mealId != null){
                     updateSnapMealsSave((snapRecipesList))
                 }else{
@@ -390,8 +385,7 @@ class MealScanResultFragment: BaseFragment<FragmentMealScanResultsBinding>() {
 //                    commit()
 //                }
             }else{
-                startActivity(Intent(context, HomeDashboardActivity::class.java))
-                requireActivity().finish()
+                ratingMealLogDialog()
             }
         }
 
@@ -418,7 +412,6 @@ class MealScanResultFragment: BaseFragment<FragmentMealScanResultsBinding>() {
                 view.findViewById<View>(R.id.view_micro).visibility = View.VISIBLE
             }
         }
-        CommonAPICall.updateChecklistStatus(requireContext(), "meal_snap", AppConstants.CHECKLIST_COMPLETED)
     }
 
     private fun onMicroNutrientsList (nutrition: ArrayList<SearchResultItem>){
@@ -926,6 +919,23 @@ class MealScanResultFragment: BaseFragment<FragmentMealScanResultsBinding>() {
                         }
                         val mealData = response.body()?.message
                         Toast.makeText(activity, mealData, Toast.LENGTH_SHORT).show()
+                        val moduleName = arguments?.getString("ModuleName").toString()
+                        if (moduleName.contentEquals("EatRight")){
+                            val fragment = HomeBottomTabFragment()
+                            val args = Bundle()
+                            args.putString("ModuleName", "EatRight")
+                            fragment.arguments = args
+                            requireActivity().supportFragmentManager.beginTransaction().apply {
+                                replace(R.id.flFragment, fragment, "landing")
+                                addToBackStack("landing")
+                                commit()
+                            }
+                        }else{
+                            val mealId = response.body()?.inserted_ids?.meal_log_id?: ""
+                            CommonAPICall.updateChecklistStatus(requireContext(), mealId, AppConstants.CHECKLIST_COMPLETED)
+                            startActivity(Intent(context, HomeDashboardActivity::class.java))
+                            requireActivity().finish()
+                        }
                     } else {
                         Log.e("Error", "Response not successful: ${response.errorBody()?.string()}")
                         Toast.makeText(activity, "Something went wrong", Toast.LENGTH_SHORT).show()
@@ -1061,6 +1071,16 @@ class MealScanResultFragment: BaseFragment<FragmentMealScanResultsBinding>() {
         })
     }
 
+    private fun ratingMealLogDialog() {
+        val ratingMealBottomSheet = RatingMealBottomSheet()
+        ratingMealBottomSheet.isCancelable = true
+        val bundle = Bundle()
+        bundle.putBoolean("test",false)
+        ratingMealBottomSheet.arguments = bundle
+        parentFragment.let { ratingMealBottomSheet.show(childFragmentManager, "RatingMealBottomSheet") }
+     //   sharedPreferenceManager.setFirstTimeUserForSnapMealRating(true)
+    }
+
     fun showLoader(view: View) {
         loadingOverlay = view.findViewById(R.id.loading_overlay)
         loadingOverlay?.visibility = View.VISIBLE
@@ -1068,5 +1088,9 @@ class MealScanResultFragment: BaseFragment<FragmentMealScanResultsBinding>() {
     fun dismissLoader(view: View) {
         loadingOverlay = view.findViewById(R.id.loading_overlay)
         loadingOverlay?.visibility = View.GONE
+    }
+
+    override fun onSnapMealRating(rating: Double) {
+        createSnapMealLog(snapRecipesList, false)
     }
 }
